6ebfec696b80482a9004f0ab2f61c200bb764182
GrahamKirwan <graham.kirwan4@gmail.com>
2021-04-21 22:41:11 -0400

    stateful lists and keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,45 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
-
-  const [filteredYear, setFilteredYear] = useState('2021');
-
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const yearChangeDataHandler = (year) => {
     setFilteredYear(year);
-  }
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onYearChange={yearChangeDataHandler}></ExpenseFilter>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+      <ExpenseFilter
+        selected={filteredYear}
+        onYearChange={yearChangeDataHandler}
+      ></ExpenseFilter>
+
+
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
     </Card>
   );
 }
-
 
 export default Expenses;
